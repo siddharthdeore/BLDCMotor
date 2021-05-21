f4a060983b11c4041f4f73d04cab888428d55143
SIDDHARTH DEORE <siddharthdeore@gmail.com>
2021-05-21 02:47:59 +0200

    Update algo.cpp
    Corrected algorithm and transformations!

--- utils/algo.cpp
+++ utils/algo.cpp
@@ -1,27 +1,55 @@
+/* Field Oriented Control Algorithm */
 void foc(double a, double b, double c, double theta, double q_ref, double &u double &v double &w) {
-    // Clarke Transform
-    double alpha = a;
-    double beta  = 0.5774*(a+2*b);
-
-    // Original Park Transform
-
     double ct = cos(theta);
     double st = sin(theta);
-    double ctm2pi3 = cos(theta-2*pi/3);
-    double stm2pi3 = sin(theta-2*pi/3);
+ 
+    /*  Clarke Transform - abc to alpha beta */
+    alpha = ((2*a) - b - c)/3;
+    beta  = (b-c)*0.5773;
+    /* Clarke to Park Transform - alpha beta to dq */
+    // phase a alinged with q axis
+    //d = alpha*st - beta*ct;
+    //q = alpha*ct + beta*st;
+    // phase a aligned with d axis
+     d = alpha*ct + beta*st;
+     q =-alpha*st + beta*ct;
+    
+    // flux PI Regulator
+    d=0-d;
+    q=q_ref-q;
+    /* Park to Clarke Transform - alpha beta to dq */
+    // phase a alinged with q axis
+    //alpha = d*st+q*ct; 
+    //beta = -d*ct+q*st;
+    // phase a aligned with d axis
+    alpha = d*ct-q*st; 
+    beta =  d*st+q*ct;
 
-    d = (alpha*ct + beta*ctm2pi3)*2/3;
-    q = (alpha*st + beta*stm2pi3)*2/3;
+    /* Inverse Clarke Transform -  alpha beta to abc */
+    u =   alpha;
+    v = - alpha*0.5 + 0.8661*beta; 
+    w = - alpha*0.5 - 0.8661*beta;
+    //
 
-    e_d = 0 - d;    // d_ref is GND
-    e_q = q_ref - q;
-
-    // inverse Park Transform
-    alpha = e_d*ct + e_q*st;
-    beta = e_d*ctm2pi3 + e_q*stm2pi3;
-
-    // inverse Clarke Transform
-    u = alpha;
-    v = -0.5*alpha+0.8660*beta;
-    w = -0.5*alpha-0.8660*beta;
 }
+/* Make sure correct phase alignment of dq frame, find out some idea to autocorrect
+// This is using direct Park
+// Park - abc to dq, phase a align to d axis
+d =   (2*a*cos(theta))/3 + (2*b*cos(theta - (2*pi)/3))/3 + (2*c*cos(theta + (2*pi)/3))/3;
+q = - (2*a*sin(theta))/3 - (2*b*sin(theta - (2*pi)/3))/3 - (2*c*sin(theta + (2*pi)/3))/3;
+
+// Park Inverse dq to abc, phase a align to d axis
+a = d*cos(theta) - q*sin(theta);
+b = d*cos(theta - (2*pi)/3) - q*sin(theta - (2*pi)/3);
+c = d*cos(theta + (2*pi)/3) - q*sin(theta + (2*pi)/3);
+
+// Park - abc to dq, phase a align to q axis
+d = (1633*a*sin(theta))/2000 + (1633*b*sin(theta - (2*pi)/3))/2000 + (1633*c*sin(theta + (2*pi)/3))/2000;
+q = (1633*a*cos(theta))/2000 + (1633*b*cos(theta - (2*pi)/3))/2000 + (1633*c*cos(theta + (2*pi)/3))/2000;
+
+// Park Inverse dq to abc, phase a align to q axis
+a =                       (1633*q*cos(theta))/2000 + (1633*d*sin(theta))/2000;
+b = (1633*q*cos(theta - (2*pi)/3))/2000 + (1633*d*sin(theta - (2*pi)/3))/2000;
+c = (1633*q*cos(theta + (2*pi)/3))/2000 + (1633*d*sin(theta + (2*pi)/3))/2000;
+
+*/
